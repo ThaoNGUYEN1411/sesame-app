8f9dacbc668acaf51391ac0866f85f0973480f59
ThaoNGUYEN1411 <phuongthaolyon@gmail.com>
2024-01-28 18:57:45 +0100

    Create Login page

---
+++ client/src/app/login/page.jsx
@@ -0,0 +1,14 @@
+import LoginForm from "@/components/LoginForm";
+// import { AuthProviderWrapper } from "@/context/authContext";
+
+const LoginPage = () => {
+	return (
+		<div>
+			{/* <AuthProviderWrapper> */}
+			<LoginForm />
+			{/* </AuthProviderWrapper> */}
+		</div>
+	);
+};
+
+export default LoginPage;
---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,81 @@
+"use client";
+import { AuthContext } from "@/context/authContext";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useContext, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+
+const LoginForm = () => {
+	const { storeToken, authenticateUser, isLoggedIn } = useContext(AuthContext);
+	const {
+		formState: { errors },
+		handleSubmit,
+		register,
+		watch,
+	} = useForm();
+	const router = useRouter();
+	const [error, setError] = useState("");
+
+	const onSubmit = async (values) => {
+		console.log(values);
+		const requestBody = values;
+
+		// créer un message de confirmation
+		// window.sessionStorage permet de stocker des chaînes de caractères dans un navigateur
+		window.sessionStorage.setItem("notice", "Account created");
+
+		axios
+			.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, requestBody)
+			.then((response) => {
+				const jwt = response.data.authToken;
+				storeToken(jwt);
+				authenticateUser();
+				router.push("/articles");
+			})
+			.catch((error) => {
+				console.log("error", error);
+				setError(error.response.data.message);
+			});
+	};
+
+	// observateur de la saisie
+	useEffect(() => {
+		// const observer = watch((values) => console.log(values));
+		const observer = watch((values) => null);
+
+		return () => observer.unsubscribe();
+	}, [watch]);
+
+	return (
+		<div className="centred-grid full-height">
+			<div className="shadow-box padding-5 rounded-box border-top-4 border-green-400">
+				<h1 className="extra-large-text bold my-4">Login</h1>
+				{error ? <h3> {error} </h3> : ""}
+				<form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3">
+					<input
+						placeholder="Email"
+						type="email"
+						{...register("email", { required: "Email is required" })}
+					/>
+					<small>{errors.email?.message}</small>
+					<input
+						type="password"
+						{...register("password", { required: "Password is required" })}
+						placeholder="Password"
+					/>
+					<small>{errors.password?.message}</small>
+					<button className="register-btn">Login</button>
+					{/* <input type="submit" className="register-btn" /> */}
+					{/* {error && <div className="error-message">{error}</div>} */}
+
+					<Link className="login-link" href={"/"}>
+						Already have an account? <span className="underline">Login</span>
+					</Link>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+export default LoginForm;
